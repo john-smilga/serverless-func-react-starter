4c182a41c401485f19c8673fc15cb19e0a374634
john-smilga <johnsmilga@gmail.com>
2021-01-26 10:14:05 -0800

    removed extra code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Product from './Product'
 ReactDOM.render(
   <React.StrictMode>
     <App />
