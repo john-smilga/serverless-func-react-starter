71617d68aa48fff9ab9e538fae736156eb7c3a80
john-smilga <johnsmilga@gmail.com>
2021-01-26 08:28:44 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h2>React and Serverless Functions</h2>
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Product from './Product'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
